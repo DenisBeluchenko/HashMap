ac86ce45545de443c8c74b425afa23600e275aa4
Denis Beluchenko <dzibis@gmail.com>
2022-11-24 21:46:15 +0700

    final

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,2 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class WordsChecker {
+    protected String text;
+    Set<String> words =new HashSet<>();
+
+    public WordsChecker(String text) {
+        String[] parts=text.split("\\P{IsAlphabetic}+");
+        for (String word:parts){
+            words.add(word);
+        }
+    }
+
+    public boolean hasWord(String word) {
+        return words.contains(word);
+    }
 }
